b2305f54831ea4589a3d0f72a0e747b822612bce
ThePlayger <denys.smyrnov.2006@gmail.com>
2024-10-15 22:57:42 +0200

    Final Final

--- HomeWork.cpp
+++ HomeWork.cpp
@@ -12,7 +12,7 @@ public:
     string SecondName;
     int Score;
 
-    // Сlass properties
+    // Сlass constructor
     Person(const string P_FirstName, const string P_SecondName, int P_score)
         : FirstName(P_FirstName), SecondName(P_SecondName), Score(P_score) {}
 };
@@ -20,7 +20,7 @@ public:
 // Function to output all objects
 void CoutAllPersons(vector<Person> persons)
 {
-    size_t i = 0;
+    unsigned long i = 0;
     cout << "List of people:" << endl;
     for (i = 0; i < persons.size(); i++)
     {
@@ -47,15 +47,12 @@ void addPerson(vector<Person> &persons)
     cin >> score;
 
     persons.push_back(Person(firstName, secondName, score));
-    // OR
-    // persons.emplace_back(firstName, secondName, score);
 }
 
 // Function for changing the object
 void changePerson(vector<Person> &persons)
 {
-    // WARNING: that about this comment?
-    size_t i = 0; // size_t becouse compare with vector size
+    unsigned long i = 0;
     cout << "Enter the number of the person you want to modify: ";
     cin >> i;
 
@@ -80,24 +77,20 @@ void changePerson(vector<Person> &persons)
 // Function to delete an object
 void removePerson(vector<Person> &persons)
 {
-    size_t i;
+    unsigned long i = 0;
     cout << "Enter the number of the person you want to delete: ";
     cin >> i;
 
     if (i < 1 || i > persons.size()) // check if User wrote correct index line
     {
         cout << "Invalid person number!" << endl;
-        // WARNING: that happen after error?
     }
 
     i--; // Decrease by 1, the user enters from 1, but indexing starts from 0
-
-    // WARNING: that is 'persons.begin()'?
-
     persons.erase(persons.begin() + i);
 }
 
-bool continueChanging()
+bool continueChanging(vector<Person> &persons)
 {
     cout << "Continue changing list? Yes (y) / No (n): ";
     char Y_N;
@@ -106,7 +99,11 @@ bool continueChanging()
     {
         return false;
     }
-    return true;
+    else
+    {
+        CoutAllPersons(persons); // Output of all created objects
+        return 0;
+    }
 }
 
 int main()
@@ -124,8 +121,9 @@ int main()
         if (!file.is_open()) // Checking if the fileName is Empty
         {
             cout << "Error opening file!" << endl;
+            main();
         }
-
+        cout << "━─━─━━─━━━─━─━─━━─━─━━─━━━─━─━─━" << endl;
         string line;
         int lineIndex = 0;
 
@@ -149,7 +147,6 @@ int main()
                 cout << "Error: Incorrect data! => " << lineIndex << endl;
                 continue;
             }
-            // WARNING: what is this???
             persons.push_back(Person(firstName, secondName, score));
         }
 
@@ -172,7 +169,7 @@ int main()
             if (choice == 'c')
             {
                 changePerson(persons);
-                if (continueChanging())
+                if (continueChanging(persons))
                 {
                     break;
                 }
@@ -180,7 +177,7 @@ int main()
             else if (choice == 'a')
             {
                 addPerson(persons);
-                if (continueChanging())
+                if (continueChanging(persons))
                 {
                     break;
                 }
@@ -188,7 +185,7 @@ int main()
             else if (choice == 'r')
             {
                 removePerson(persons);
-                if (continueChanging())
+                if (continueChanging(persons))
                 {
                     break;
                 }
@@ -209,7 +206,6 @@ int main()
     }
 
     CoutAllPersons(persons); // Output the new list
-    // Write to output file
     try
     {
         cout << "Enter the output file name: ";
@@ -222,7 +218,7 @@ int main()
             return 1;
         }
 
-        for (size_t i = 0; i < persons.size(); i++)
+        for (unsigned long i = 0; i < persons.size(); i++)
         {
             Person person = persons[i];
             outputFile << person.FirstName << " " << person.SecondName << " " << person.Score << endl;
@@ -234,19 +230,20 @@ int main()
     {
         cout << error.what() << endl;
     }
-    cout << "    ⊂ヽ" << endl;
-    cout << "   　 ＼＼   Λ＿Λ" << endl;
-    cout << "    　　 ＼( ˘ω˘ )" << endl;
-    cout << "    　　　 >　⌒ヽ" << endl;
-    cout << "    　　　/ 　 へ＼" << endl;
-    cout << "    　　 /　　/　＼＼" << endl;
-    cout << "    　　 ﾚ　ノ　　 ヽ_つ" << endl;
-    cout << "    　　/　/" << endl;
-    cout << "    　 /　/|" << endl;
-    cout << "    　(　(ヽ" << endl;
-    cout << "    　|　|、＼" << endl;
-    cout << "    　| 丿 ＼ ⌒)" << endl;
-    cout << "    　| |　　) /" << endl;
-    cout << "     ノ )   Lﾉ" << endl;
-    cout << "    (_／" << endl;
+
+    cout << "set the program again? Yes (y) / No (n): ";
+    char Y_N;
+    cin >> Y_N;
+    if (Y_N == 'y')
+    {
+        main();
+    }
+    else if (Y_N == 'n')
+    {
+        return 1;
+    }
+    else
+    {
+        cout << "No command found, try again! " << endl;
+    }
 }
